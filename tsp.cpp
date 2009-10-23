1b80c1cdb51ee35964ac4b8eb8a453a670e083f5
xun <xun@xvid.se>
2009-10-24 01:26:27 +0200

    Fick poängen att gå upp med .15 poäng ://

--- tsp.cpp
+++ tsp.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
 
+#define DEBUG 1
+
 struct node {
     float x;
     float y;
@@ -44,13 +46,18 @@ void swap(int x, int y, int  path[]) {
     path[y] = tmp;
 }
 
+void printPath(node * nodes[], int path[], int numNodes) {
+    for (int i = 0; i < numNodes; i++) {
+        std::cout << "path[" << i << "] => " << path[i] << std::endl;
+    }
+}
 
 void reverse(int a, int b, int path[]) {
-    while (b-a > 1) {
-        // std::cerr << "swap(" << a << "," << b << ")" << std::endl;
+    b++;
+    for (;a<--b;a++) {
+        if (DEBUG >= 2) 
+            std::cerr << "swap(" << a << "," << b << ")" << std::endl;
         swap(a,b, path);
-        a++;
-        b--;
     }
 }
 
@@ -73,25 +80,34 @@ int main() {
 
 
     int path[numNodes];
-    bool checked[numNodes];
 
     // just some path
     for (int i = 0; i < numNodes; i++) {
         path[i] = i;
-        checked[i] = false;
     }
 
     // 2-opt, i hope
     for (int i = 0; i < numNodes-1; i++) {
-        for (int n = i; n < numNodes-1; n++) {
+        for (int n = i+3; n < numNodes-1; n++) {
             if (distance(*nodes[path[i+0]], *nodes[path[i+1]]) + distance(*nodes[path[n+0]], *nodes[path[n+1]]) >
-                    distance(*nodes[path[i+1]], *nodes[path[n+0]]) + distance(*nodes[path[i+0]], *nodes[path[n+1]])) {
-                reverse(i, n, path);
+                    distance(*nodes[path[i+0]], *nodes[path[n+0]]) + distance(*nodes[path[i+1]], *nodes[path[n+1]])) {
+
+                if (DEBUG) {
+                    std::cerr << "\treverse(" << i+1 << "," << n << ")\t=> " << pathLength(nodes, path, numNodes);
+                }
+                reverse(i+1, n+0, path);
+                if (DEBUG) {
+                    std::cerr << " - " << pathLength(nodes, path, numNodes) << std::endl;
+                    if (DEBUG >= 2)
+                        printPath(nodes, path, numNodes);
+                }
+                // i = 0;
             }
         }
     }
 
-    // reverse(0,numNodes-1, path);
+    // reverse(2,5, path);
+    // printPath(nodes, path, numNodes);
 
     // output result
     for (int i = 0; i < numNodes; i++) {
