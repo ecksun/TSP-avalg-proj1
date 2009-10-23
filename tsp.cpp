0555fdf0216d2dad27387edf53ae7fa6c54ba88a
xun <xun@xvid.se>
2009-10-23 16:23:59 +0200

    La till två funktioner som kan räkna ut avstånd mellan både noder och två uppsättningar koordinater

--- tsp.cpp
+++ tsp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 struct node {
     float x;
@@ -14,8 +15,13 @@ std::ostream & operator<<(std::ostream & os, const node & d) {
 }       
 
 
+
+float distance(float x1, float y1, float x2, float y2); 
+
+float distance(node n1, node n2);
+
+using namespace std;
 int main() {
-    using namespace std;
     int numNodes;
     scanf("%d", &numNodes);
     cerr << "numNodes:\t" << numNodes << endl;
@@ -31,12 +37,34 @@ int main() {
     // testa något greedy
     
 
+    int path[numNodes];
+
+    for (int i = 0; i < numNodes; i++) {
+        path[i] = i;
+    }
+
+
+    for (int i = 0; i < numNodes; i++) {
+        
+    }
+
+    // cout << distance(0.0f,0.0f,3.0f,4.0f) << endl;
+
     // for (int i = 0; i < numNodes; i++) {
         // cout << *nodes[i] << endl;
     // }
 
+
     for (int i = 0; i < numNodes; i++) {
-        cout << i << endl;
+        cout << path[i] << endl;
     }
 
+}
+
+float distance(float x1, float y1, float x2, float y2) {
+    return sqrt(pow((x2-x1), 2) + pow((y2-y1),2));
+}
+
+float distance(node n1, node n2) {
+    return distance(n1.x, n1.y, n2.x, n2.y);
 }
