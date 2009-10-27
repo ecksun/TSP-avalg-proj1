3bac8d9c1825557b411490bf9e3ee6edaaf30f28
Joel Pettersson <joelpet@joelpet.nodensa>
2009-10-27 14:56:25 +0100

    Skapade Node-klassen i java.

---
+++ Node.java
@@ -0,0 +1,18 @@
+class Node {
+
+    public float x;
+    public float y;
+
+    /**
+     * Constructs a new node.
+     *
+     * @param x The x coordinate
+     * @param y The y coordinate
+     */
+    Node(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
