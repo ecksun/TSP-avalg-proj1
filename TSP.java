64d638b30d4e04dbdf51252587f5b9ebb2e5e0c8
Joel Pettersson <joelpet@joelpet.nodensa>
2009-10-28 13:51:39 +0100

    Lagade TSP.

--- TSP.java
+++ TSP.java
@@ -10,9 +10,15 @@ public class TSP {
 
     Tour tour;
 
-    public final boolean DEBUG = true;
+    public final int DEBUG = 1;
 
-    private int neighborsToCheck = 11;
+    private int neighborsToCheck = 7;
+
+    private void dbg(Object o) {
+        if (DEBUG > 2) {
+            System.err.println(o);
+        }
+    }
 
     public static void main (String[] argv) {
         new TSP();
@@ -23,32 +29,24 @@ public class TSP {
         nodes = new Node[numNodes];
         tour = new Tour(numNodes);
         long time;
-        if (DEBUG)
+        if (DEBUG > 0)
             time = System.currentTimeMillis();
 
         readNodes();
 
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("readNodes()         " + (System.currentTimeMillis() - time));	
             time = System.currentTimeMillis();
         }
 
         createDistance();
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("createDistance()	" + (System.currentTimeMillis() - time));
             time = System.currentTimeMillis();
         }
 
-        // 
-        // for (int i = 0; i < numNodes; i++) {
-        // for (int j = 0; j < numNodes; j++) {
-        // System.err.print(distance[i][j] + "\t");
-        // }
-        // System.out.println();
-        // }
-
         createNeighbors();  
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("createNeighbors()	" + (System.currentTimeMillis() - time));
             time = System.currentTimeMillis();
         }
@@ -56,19 +54,19 @@ public class TSP {
         printNeighbors();
 
         NNPath();
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("NNPath()	        " + (System.currentTimeMillis() - time));
             time = System.currentTimeMillis();
         }
 
         twoOpt();
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("twoOpt()	        " + (System.currentTimeMillis() - time));
             time = System.currentTimeMillis();
         }
 
         printTour();
-        if (DEBUG) {
+        if (DEBUG > 0) {
             System.err.println("printTour()	        " + (System.currentTimeMillis() - time));
             System.err.println("tour length:  " + tour.length(this));
         }
@@ -144,6 +142,8 @@ public class TSP {
      * Print all neighbors in a matrix style kinda way
      */
     void printNeighbors() {
+        if (DEBUG <= 0) return;
+
         System.err.println("=== Neighbors ===");
         for (int i = 0; i < numNodes; i++) {
             for (int j = 0; j < neighborsToCheck; j++) {
@@ -162,10 +162,8 @@ public class TSP {
 
     double distance(int a, int b) {
         if (b < a) {
-            // System.err.printf("distance(%d, %d) = %f\n", a, b, distance[b][a]);
             return distance[b][a];
         }
-        // System.err.printf("distance(%d, %d) = %f\n", a, b, distance[a][b]);
         return distance[a][b];
     }
 
@@ -212,26 +210,13 @@ improve: // restart
 
                     if (distance(c1, nc1) + distance(c2, nc2) >
                             distance(c1, c2) + distance(nc1, nc2)) {
-                        System.err.printf("Working with c1(%d), nc1(%d), c2(%d), nc2(%d)\n", c1, nc1, c2, nc2);
-                        System.err.printf("Före (%d, %d): %s\n", nc1, c2, tour.length(this));
+                        dbg(String.format("Working with c1(%d), nc1(%d), c2(%d), nc2(%d)\n", c1, nc1, c2, nc2));
+                        dbg(String.format("Före (%d, %d): %s\n", nc1, c2, tour.length(this)));
                         improvement = true;
 
-                        // Det utkommenderande nedan är hesselbys kod
-                        // int ai = tour.getPos(nc1);
-                        // int bi = tour.getPos(c2);
-                        // int amli = tour.getPos(tour.getPrevNode(nc1));
-                        // int bpli = tour.getPos(tour.getNextNode(c2));
+                        reverse(tour.getPos(nc1), tour.getPos(c2));
+                        dbg(String.format("efter (%d, %d): %s\n", nc1, c2, tour.length(this)));
                         
-                        // if (Math.abs(bi - ai) < Math.abs(bpli - amli)) {
-                            // reverse(ai, bi);
-                        // }
-                        // else {
-                            // reverse(bpli, amli);
-                        // }
-
-                        reverse(nc1, c2);
-                        System.err.printf("efter (%d, %d): %s\n", nc1, c2, tour.length(this));
-                        // reverse(pos[i]+1, neighbors[pos[i]][n]);
                         continue improve;
                     }
 
@@ -245,7 +230,7 @@ improve: // restart
         if (a > b)
             reverse(b, a);
         else {
-            System.err.printf("Före: (%d, %d)\t \n%s", a, b, tour);
+            dbg(String.format("Före: (%d, %d)\t \n%s", a, b, tour));
 
             while (a < b) {
                 // wraparound
@@ -260,35 +245,7 @@ improve: // restart
                 b--;
             }
 
-            System.err.printf("efter: (%d, %d)\t \n%s", a, b, tour);
+            dbg(String.format("efter: (%d, %d)\t \n%s", a, b, tour));
         }
     }
-    /*
-     * hesselbys kod
-     */
-    // void reverse(int start, int end)
-    // {
-        // //System.err.println("reverse\t"+start+"\t"+end);
-        // int len = end - start;
-// 
-        // if (len < 0) // vi har en wraparound
-        // {
-            // len += numNodes;
-        // }
-// 
-        // len = (len+1)/2; // vi behöver bara swappa "hälften av längden" ggr, eftersom vi tar 2 element per swap (+1 för att inte udda antal element ska lämnas kvar)
-// 
-        // for (int k = 0; k < len; k++)
-        // {
-            // tour.swap(start, end);
-// 
-            // // wraparound för start & end
-            // if (++start == numNodes)
-                // start = 0;
-// 
-            // if (--end <= 0)
-                // end = numNodes-1;
-        // }
-    // }
-
 }
